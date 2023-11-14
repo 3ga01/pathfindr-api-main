e72f136287418a8a5401e121b522f78d1b8210ed
Emmanuel Mireku Omari <omariemmanuel91@gmail.com>
2023-11-14 22:31:14 +0000

    origins set

--- src/main/java/com/example/pathfindr/controller/MainController.java
+++ src/main/java/com/example/pathfindr/controller/MainController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -52,6 +53,7 @@ public class MainController {
     @Autowired
     MatchingService matchingService;
 
+    @CrossOrigin
     @PostMapping("/signUpStudent")
     public ResponseEntity<String> addNewUser(@RequestBody Student student) {
 
